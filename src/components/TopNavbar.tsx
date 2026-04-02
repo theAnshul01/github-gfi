1dec892cd6d9269122fbe159775dab760bdea270
Anshul <anshulporwal2001@gmail.com>
2026-04-02 23:10:21 +0530

    navbar: added side navbar

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import { LuSearchCode } from "react-icons/lu";
->>>>>>> 6d41f23 (navbar: added top navbar)
 import { NavLink } from "react-router-dom";
 
 const TopNavbar = () => {
