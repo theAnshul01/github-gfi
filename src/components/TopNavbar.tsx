d5bc0120d4565a206510cd06cd9c27d2665f582a
Anshul <anshulporwal2001@gmail.com>
2026-04-02 22:18:55 +0530

    navbar: added top navbar

---
+++ src/components/TopNavbar.tsx
@@ -0,0 +1,44 @@
+import { LuSearchCode } from "react-icons/lu";
+import { NavLink } from "react-router-dom";
+
+const TopNavbar = () => {
+
+  return (
+    <nav>
+      <div className="bg-slate-900 mb-2 px-2 font-space">
+        <div className="flex items-center justify-between">
+          <h1 className="text-[#14e00d] font-semibold py-4 px-3 text-xl">
+            [GOOD_FIRST_ISSUES]
+          </h1>
+
+          <div className="flex items-center justify-between gap-4 text-sm cursor-pointer">
+            <NavLink to="/" className={({isActive}) => `hover:underline-offset-4 hover:underline ${isActive ? "text-green-500" : "text-gray-600"}`}>home</NavLink>
+            <NavLink to="/issues" className={({isActive}) => `hover:underline-offset-4 hover:underline ${isActive ? "text-green-500" : "text-gray-600"}`}>Issues</NavLink>
+
+            {/* // TODO:  navlink destination to be updated  */}
+
+            <NavLink to="bookmark" className={({ isActive }) => `hover:underline-offset-4 hover:underline ${isActive ? "text-green-500" : "text-gray-600"}`}>bookmarks</NavLink>
+          </div>
+
+          <div className="flex items-end justify-between gap-3 text-[#14e00d]">
+            <div className="flex items-center justify-between">
+              <label htmlFor="repo-search">
+                <LuSearchCode className="border text-green-600 border-gray-700 text-xl rounded-sm cursor-pointer"/>
+              </label>
+              <input
+                id="repo-search"
+                type="text"
+                placeholder="$ grep issues --label='good-first-issues'"
+                className="placeholder-green-800 border border-gray-700 bg-slate-900 rounded-sm px-2 focus:outline-none focus:ring-1 focus:ring-green-500 text-sm"
+              />
+            </div>
+            <p>[Session ID]</p>
+            <p>User Profile</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default TopNavbar
