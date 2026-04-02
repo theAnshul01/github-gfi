6d41f233c2318e787b504e2e5a38ef500d1ba2d8
Anshul <anshulporwal2001@gmail.com>
2026-04-02 22:18:55 +0530

    navbar: added top navbar

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,16 @@ import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ErrorBoundary from "./components/ErrorBoundary.tsx"
 import AuthCallback from "./components/AuthCallback.tsx"
+import TopNavbar from './components/TopNavbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ErrorBoundary>
         <Routes>
-          <Route path="/auth/callback" element={<AuthCallback />} />
-          <Route path="/*" element={<App />} />
+          <Route path="/" element={<TopNavbar/>}/>
+          {/* <Route path="/auth/callback" element={<AuthCallback />} /> */}
+          {/* <Route path="/*" element={<App />} /> */}
         </Routes>
       </ErrorBoundary>
     </BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        space: ["Space Grotesk", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
