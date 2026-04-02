9bb0c2660d23aa1654095d305447e70c4839cef6
Anshul <anshulporwal2001@gmail.com>
2026-04-02 23:10:21 +0530

    navbar: added side navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/700.css";
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/600.css";
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ErrorBoundary from "./components/ErrorBoundary.tsx"
-import AuthCallback from "./components/AuthCallback.tsx"
-import TopNavbar from './components/TopNavbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ErrorBoundary>
         <Routes>
-          <Route path="/" element={<TopNavbar/>}/>
-          {/* <Route path="/auth/callback" element={<AuthCallback />} /> */}
-          {/* <Route path="/*" element={<App />} /> */}
+          <Route path="/*" element={<App />} />
         </Routes>
       </ErrorBoundary>
     </BrowserRouter>
