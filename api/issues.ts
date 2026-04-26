7e2d1351fa42e9b8f967539eb4eebbfa024dd3a8
Anshul <anshulporwal2001@gmail.com>
2026-04-26 18:48:17 +0530

    fix: build

--- api/issues.ts
+++ api/issues.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
-import { redis } from "../src/lib/redis"
+import { redis } from "../src/lib/redis.js"
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const { page = 1, per_page = 20, language, label, search } = req.query
