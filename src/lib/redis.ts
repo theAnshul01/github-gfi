b728c0857bc9009226a08282a60d0f982967fefd
Anshul <anshulporwal2001@gmail.com>
2026-04-26 17:27:24 +0530

    fix: import and env variables

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis"
 
 export const redis = new Redis({
-    url: import.meta.env.VITE_UPSTASH_REDIS_REST_URL!,
-    token: import.meta.env.VITE_UPSTASH_REDIS_REST_TOKEN!,
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 })
