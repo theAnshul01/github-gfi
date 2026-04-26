89dc9aa96359bf3f92d39e022829fffd849decc1
Anshul <anshulporwal2001@gmail.com>
2026-04-26 16:18:36 +0530

    chore: added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Footer from "./components/Footer"
 import Sidebar from "./components/Sidebar"
 import Home from "./pages/Home"
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
-
-  
 
   return (
     <>
       <Sidebar/>
       <Home />
       <Footer/>
+      <Analytics />
     </>
   )
 }
